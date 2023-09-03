908014fca5dadec2ec3e3d26fc0e452571fc0fe7
Macgithub13 <macgames444@gmail.com>
2023-09-03 15:04:54 -0300

    post feito, na pagina cliente junto da estilização

--- src/api/clienteAPI.js
+++ src/api/clienteAPI.js
@@ -1,27 +1,23 @@
 import axios from 'axios';
 
-const apiCliente = axios.create({
-
-    URL:'http://localhost:5000/locadora'
-});
-
-export async function cadastrarCliente(nome,email,telefone,cpf,cnh){
-
-    const resp = await apiCliente.post('/cliente', {
-
-        nome:nome,
-        email:email,
-        telefone:telefone,
-        CPF:cpf,
-        CNH:cnh
-    });
+export async function cadastarClientes(nome,email,telefone,cpf,cnh){
+ 
+    let cliente={
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        cpf: cpf,
+        cnh: cnh
+    };
+        
+    let resp=await axios.post('http://localhost:5000/locadora/cliente', cliente) 
 
     return resp.data;
 }
 
 export async function procurarClientes(nome){
 
-    const resp = await apiCliente.get(`/cliente/nome?nome=${nome}`);
+    const resp = await axios.get(`http://localhost:5000/cliente/nome?nome=${nome}`);
 
     return resp.data;
 }
